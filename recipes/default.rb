792b655e53c32368e8df10ab95689641542955b8
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-04-03 13:13:08 +0100

    Basic install of Chef Server

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,35 @@
 # Cookbook Name:: chef-server
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Protec Innovations
 #
-# All rights reserved - Do Not Redistribute
+# Licenced under the BSD
 #
+
+node.set_unless['chef_server']['rabbitmq_password'] = secure_password
+node.set_unless['chef_server']['webui_password'] = secure_password
+node.save
+
+directory "/var/cache/local/preseeding" do
+    owner "root"
+    group node['chef_server']['root_group']
+    mode 0755
+    recursive true
+end
+
+execute "preseed chef-server" do
+    command "debconf-set-selections /var/cache/local/preseeding/chef-server.seed"
+    action :nothing
+end
+
+template "/var/cache/local/preseeding/chef-server.seed" do
+    source "chef-server.seed.erb"
+    owner "root"
+    group node['chef_server']['root_group']
+    mode "0600"
+    notifies :run, "execute[preseed chef-server]", :immediately
+end
+
+package "chef-server" do
+    action :install
+end
