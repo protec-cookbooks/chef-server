1aa7ac6b26a3b0065e7a6a03e62c82481f7a5565
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-04-03 13:51:32 +0100

    Update Chef Repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,17 @@
 # Licenced under the BSD
 #
 
-node.set_unless['chef_server']['rabbitmq_password'] = secure_password
-node.set_unless['chef_server']['webui_password'] = secure_password
-node.save
+if not node.chef_server.attribute?('rabbitmq_password')
+    node.set_unless['chef_server']['rabbitmq_password'] = secure_password
+    Chef::Log.info('RabbitMQ Password: ' + node['chef_server']['rabbitmq_password'])
+    node.save
+end
+
+if not node.chef_server.attribute?('webui_password')
+    node.set_unless['chef_server']['webui_password'] = secure_password
+    Chef::Log.info('WebUI Password: ' + node['chef_server']['webui_password'])
+    node.save
+end
 
 directory "/var/cache/local/preseeding" do
     owner "root"
