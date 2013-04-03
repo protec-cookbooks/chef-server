6130731c720a95693c9395190fae0041a93b2f89
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-04-03 14:38:10 +0100

    Remove node.save

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,13 +12,11 @@
 if not node.chef_server.attribute?('rabbitmq_password')
     node.set_unless['chef_server']['rabbitmq_password'] = secure_password
     Chef::Log.info('RabbitMQ Password: ' + node['chef_server']['rabbitmq_password'])
-    node.save
 end
 
 if not node.chef_server.attribute?('webui_password')
     node.set_unless['chef_server']['webui_password'] = secure_password
     Chef::Log.info('WebUI Password: ' + node['chef_server']['webui_password'])
-    node.save
 end
 
 directory "/var/cache/local/preseeding" do
